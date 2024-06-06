058f8316b938b0ab9ce7224ac767c73e88d6592b
CaioTakabatake <caio.soldera.taka@gmail.com>
2024-06-05 21:00:47 -0300

    update api url

--- script.js
+++ script.js
@@ -3,26 +3,28 @@ const pass = document.querySelector("#password");
 const form = document.querySelector("form");
 
 form.onsubmit = async (event) => {
-    event.preventDefault();
-    const userValue = user.value;
-    const passValue = pass.value;
+  event.preventDefault();
+  const userValue = user.value;
+  const passValue = pass.value;
 
-    if (userValue.includes("@")) {
-        const response = await fetch(`https://auto-spreadsheets-api.vercel.app/login`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                email: userValue,
-                password: passValue
-            })
-        });
+  if (userValue.includes("@")) {
+    const response = await fetch(
+      `https://auto-spreadsheets-api.vercel.app/api/login`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: userValue,
+          password: passValue,
+        }),
+      }
+    );
 
-
-        console.log(response);
-        console.log(response.body);
-    } else {
-        console.log("teste")
-    }
+    console.log(response);
+    console.log(response.body);
+  } else {
+    console.log("teste");
+  }
 };
