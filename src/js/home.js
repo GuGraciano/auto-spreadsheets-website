c343620995c94a4f26b8a9dd39d5a647dc8cb7d2
GuGraciano <gustavograciano477@gmail.com>
2024-09-13 15:58:49 -0300

    update

--- src/js/home.js
+++ src/js/home.js
@@ -1,13 +1,19 @@
 document.addEventListener("DOMContentLoaded", async function () {
     const userString = localStorage.getItem("user");
+    const userAccountContainer = document.querySelector('.user-account');
 
     if (!userString) {
-        location.href = "./login.html";
+        // Se o usuário não estiver logado, exiba o link de login
+        userAccountContainer.innerHTML = `
+            <img src="src/img/blank-profile-picture-973460_960_720.webp" alt="Perfil" class="profile-img">
+            <a href="login.html">Login</a>
+        `;
         return;
     }
 
     const user = JSON.parse(userString);
 
+    // Re-login para verificar as permissões do usuário
     async function reLogin() {
         const requestOptions = {
             method: "POST",
@@ -19,10 +25,7 @@ document.addEventListener("DOMContentLoaded", async function () {
         };
 
         try {
-            const response = await fetch(
-                "https://auto-spreadsheets-api.vercel.app/api/login",
-                requestOptions
-            );
+            const response = await fetch("https://auto-spreadsheets-api.vercel.app/api/login", requestOptions);
 
             if (!response.ok) {
                 throw new Error("Network response was not ok");
@@ -34,30 +37,23 @@ document.addEventListener("DOMContentLoaded", async function () {
             if (data.status) {
                 const { userName, password, email, roles } = data.userData;
 
-                if (roles.includes("adm")) {
-                    const userObject = {
-                        userName,
-                        password,
-                        email,
-                        roles,
-                    };
+                // Atualize o objeto do usuário no localStorage
+                const userObject = {
+                    userName,
+                    password,
+                    email,
+                    roles,
+                };
+                localStorage.setItem("user", JSON.stringify(userObject));
 
-                    localStorage.setItem("user", JSON.stringify(userObject));
-
-                    const rolesStr = roles.join(", ");
-                    notify("success", `Logged in as ${userName} with roles: ${rolesStr}`);
-
-                    // handleLogged();
-                } else {
-                    notify("danger", `User ${userName} has no permissions`);
-                    const main = document.querySelector("main");
-                    main.style.height = "90vh";
-                    main.innerHTML = "";
-
-                    setTimeout(() => {
-                        location.href = "./login.html";
-                    }, 3000);
-                }
+                // Atualize o HTML para exibir "Minha Conta"
+                userAccountContainer.innerHTML = `
+                    <img src="src/img/blank-profile-picture-973460_960_720.webp" alt="Minha Conta" class="profile-img">
+                    <a href="conta.html"><p>Minha Conta</p></a>
+                `;
+                
+                const rolesStr = roles.join(", ");
+                notify("success", `Logged in as ${userName} with roles: ${rolesStr}`);
             } else {
                 notify("danger", data.message);
                 localStorage.removeItem("user");
@@ -67,12 +63,13 @@ document.addEventListener("DOMContentLoaded", async function () {
             console.error("There was a problem with the fetch operation:", error);
             notify("danger", "Failed to re-login. Redirecting to login...");
             localStorage.removeItem("user");
-            // location.href = "./login.html";
+            location.href = "./login.html";
         }
     }
 
     await reLogin();
 });
+
 
 let currentSlide = 0;
 const slideInterval = 3000; // Tempo em milissegundos (3 segundos)
